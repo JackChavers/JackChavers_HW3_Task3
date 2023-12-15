7eb4b6110101f3669b1f39f089906a4611acbf72
jackc <jackchavers7@gmail.com>
2023-12-15 12:22:12 -0600

    Add project files.

---
+++ JackChavers_HW3_Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Jack Chavers
+// HW3 Task3 TutitionIncrease
+
+double tuitionAmount = 12000.0;
+
+int N = 7;
+
+for(int i = 0; i < N; i++)
+{
+    tuitionAmount = tuitionAmount * (1 + 0.05);
+    string message = string.Format($"The tuition after year {i + 1} will be {tuitionAmount:C0}.");
+    Console.WriteLine(message);
+
+}
